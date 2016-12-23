c480fdbf5be6f8be0f8002d0ecb27c4cf7beba52
predatell <seryi_belyi@inbox.ru>
2016-12-23 20:28:53 +0000

    urls.py edited online with Bitbucket

--- keyedcache/urls.py
+++ keyedcache/urls.py
@@ -1,11 +1,12 @@
 """
 URLConf for Caching app
 """
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    (r'^$', views.stats_page, {}, 'keyedcache_stats'),
-    (r'^view/$', views.view_page, {}, 'keyedcache_view'),
-    (r'^delete/$', views.delete_page, {}, 'keyedcache_delete'),
+    url(r'^$', views.stats_page, {}, 'keyedcache_stats'),
+    url(r'^view/$', views.view_page, {}, 'keyedcache_view'),
+    url(r'^delete/$', views.delete_page, {}, 'keyedcache_delete'),
 ]
